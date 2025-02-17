1d4f38c8d9a6e0d807eaeac0a11df9565b91f750
sivasan2022 <grsivasankari79@gmail.com>
2025-02-17 15:48:24 +0530

    created list department feature
    Signed-off-by: sivasan2022 <grsivasankari79@gmail.com>

--- ems-frontend/src/components/HeaderComponenet.jsx
+++ ems-frontend/src/components/HeaderComponenet.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const HeaderComponent = () => {
   return (
     <div>
         <header>
-            <nav className='navbar navbar-dark bg-dark'>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
                 <a className='navbar-brand ps-2' href='#'>Employee Management System</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <NavLink className="nav-link active" to="/employees">Employees</NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/departments">Departments</NavLink>
+                    </li>
+                  </ul>
+                </div>
             </nav>
         </header>
     </div>
---
+++ ems-frontend/src/components/ListDepartmentComponent.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+import { getAllDepartments } from '../services/DepartmentService'
+
+const ListDepartmentComponent = () => {
+
+    useEffect( () => {
+        getAllDepartments().then((response) => {
+            console.log(response.data);
+            setDepartments(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    },[]);
+
+    const [departments, setDepartments] = useState([]);
+
+  return (
+    <div className='container'>
+        <h2 className='text-center'>List of Departments</h2>
+        <table className='table table-striped table-hover table-bordered'>
+            <thead>
+                <tr>
+                    <th>Department Id</th>
+                    <th>Department Name</th>
+                    <th>Department Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    departments.map(department => 
+                        <tr key={department.id}>
+                            <td> {department.id}</td>
+                            <td> {department.departmentName}</td>
+                            <td> {department.departmentDescription}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default ListDepartmentComponent
